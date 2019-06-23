c77fc3d8443c109fef51d36abbe1013db50db2c6
jwdev66 <jwdev66@gmail.com>
2019-06-23 19:51:47 +0300

    create database seeders

---
+++ database/seeds/IdeasTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class IdeasTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $contact = factory(\App\Idea::class)->create();
+    }
+}
