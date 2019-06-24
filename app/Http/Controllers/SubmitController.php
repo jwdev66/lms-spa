41d36ed0afb3615f999515020db9f06a55d491af
jwdev66 <jwdev66@gmail.com>
2019-06-25 00:19:20 +0300

    Create SubmitController.php

---
+++ app/Http/Controllers/SubmitController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\IdeaSubmitted;
+use App\Idea;
+use Illuminate\Http\Request;
+
+class SubmitController extends Controller
+{
+    public function store(Request $request)
+    {
+        $idea = Idea::findOrFail($request->id);
+        $user = auth()->user();
+
+        event(new IdeaSubmitted($user, $idea));
+
+        return 'Idea '. $idea->description .'submitted!!';
+    }
+
+
+}
