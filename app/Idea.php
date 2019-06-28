30167b5bebed9d7ded0e9500d86bac2652e28c4e
jwdev66 <jwdev66@gmail.com>
2019-06-28 03:15:32 +0300

    update model for roles

--- app/Idea.php
+++ app/Idea.php
@@ -4,7 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string title
+ * @property string description
+ */
 class Idea extends Model
 {
-    //
+    protected $fillable= ['title','description'];
+
+    public function user()
+    {
+        return $this->belongsTo(\App\User::class);
+    }
 }
