b193203f5d82ca74207ee84ec011f47f0d19b695
jwdev66 <jwdev66@gmail.com>
2019-06-28 03:04:49 +0300

    update models factory

--- database/factories/IdeaFactory.php
+++ database/factories/IdeaFactory.php
@@ -8,6 +8,13 @@ use Faker\Generator as Faker;
 $factory->define(\App\Idea::class, function (Faker $faker) {
     return [
         'title' => $faker->title,
-        'description' => $faker->text(120),
+        'description' => $faker->paragraph(1),
+        'user_id' => function () {
+            $user = factory(App\User::class)->create();
+            $role = App\Role::where('name', 'investigator')->first();
+            $user->roles()->save($role);
+
+            return $user->id;
+        }
     ];
 });
