e30f3c19b27a48ff49211442c760dc34737ac004
jwdev66 <jwdev66@gmail.com>
2019-07-05 14:19:55 +0300

    add multi langage supprt

---
+++ app/Http/Middleware/SetDefaultLocaleForUrls.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class SetDefaultLocaleForUrls
+{
+
+    public function handle($request, Closure $next)
+    {
+        URL::defaults(['locale' => $request->user()->locale]);
+        return $next($request);
+    }
+}
