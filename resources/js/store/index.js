fb9c2bb73075e6e85644955f1b2a0a21399d043f
jwdev66 <jwdev66@gmail.com>
2019-10-24 13:20:42 +0300

    feat: update store

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -6,15 +6,13 @@ Vue.use(Vuex)
 // Load store modules dynamically.
 const requireContext = require.context('./modules', false, /.*\.js$/)
 
-const modules = requireContext.keys()
-  .map(file =>
-    [file.replace(/(^.\/)|(\.js$)/g, ''), requireContext(file)]
-  )
+const modules = requireContext
+  .keys()
+  .map(file => [file.replace(/(^.\/)|(\.js$)/g, ''), requireContext(file)])
   .reduce((modules, [name, module]) => {
     if (module.namespaced === undefined) {
       module.namespaced = true
     }
-
     return { ...modules, [name]: module }
   }, {})
 
